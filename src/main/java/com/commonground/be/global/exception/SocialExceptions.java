14d575ca33220380c95c00fae0d7442bff7863f5
leebeanbin <67886181+leebeanbin@users.noreply.github.com>
2025-07-29 23:45:10 +0900

    add : social login exceptions

---
+++ src/main/java/com/commonground/be/global/exception/SocialExceptions.java
@@ -0,0 +1,27 @@
+package com.commonground.be.global.exception;
+
+import com.commonground.be.global.response.ResponseExceptionEnum;
+
+public class SocialExceptions {
+
+    public static CommonException invalidProvider() {
+        return new CommonException(ResponseExceptionEnum.INVALID_SOCIAL_PROVIDER);
+    }
+
+    public static CommonException tokenRequestFailed() {
+        return new CommonException(ResponseExceptionEnum.SOCIAL_TOKEN_REQUEST_FAILED);
+    }
+
+    public static CommonException userInfoRequestFailed() {
+        return new CommonException(ResponseExceptionEnum.SOCIAL_USER_INFO_REQUEST_FAILED);
+    }
+
+    public static CommonException invalidTokenResponse() {
+        return new CommonException(ResponseExceptionEnum.INVALID_SOCIAL_TOKEN_RESPONSE);
+    }
+
+    public static CommonException invalidUserInfoResponse() {
+        return new CommonException(ResponseExceptionEnum.INVALID_SOCIAL_USER_INFO_RESPONSE);
+    }
+
+}
